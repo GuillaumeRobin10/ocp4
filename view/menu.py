2d4c06b67c1b76c8b1150aaddee1b5209948be29
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-03-01 21:06:21 +0100

    version 0.0.0.1
    création d'un tournois par chargement d'un fichier de la db ou par création manuelle

--- view/menu.py
+++ view/menu.py
@@ -3,7 +3,9 @@
 import os
 
 from controller.display_function import display
-
+from controller.instance_loading import load_a_json_files
+from controller.files_in_database import get_datafiles
+from controller.instance_making import make_a_tournament
 
 def make_hyphens(name_menu):
     """Give it a string and return a string of hyphens equal of the giving string."""
@@ -26,6 +28,21 @@ def choice_menu(menu_string, cases, header_name=""):
     if not header_name == "":
         display_head_menu(header_name)
     return display(menu_string, "choice", cases)
+
+
+def loading_tournament_menu():
+    files = get_datafiles("databases/tournaments/*.json")
+    if files == {}:
+        print("pas de fichier disponible en base de donnée")
+    else:
+        keys = []
+        for key in files:
+            menu_string = f"Nom : {files[key]} ({key})\n"
+            keys.append(key)
+        uchoice = choice_menu(menu_string, keys, "Chargement d'un tournois")
+        tournament_file = f"databases/tournaments/{uchoice}_{files[uchoice]}.json"
+        tournament = make_a_tournament(load_a_json_files(tournament_file))
+        return tournament
 
 
 if __name__ == "__main__":
