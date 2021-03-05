ecf8682009f01881b191ebe71c573de1a728c325
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-03-06 00:41:34 +0100

    v 0.0.1
    version sans polish visuel et sans tinydb

---
+++ view/display_instance_making.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from controller.Display import display, type_str
+from controller.building import make_a_tournament, make_a_player
+from view.tools import display_head_menu
+
+
+def display_make_a_tournament():
+    os.system('cls||clear')
+    display_head_menu("tournament Building")
+    tournament_dict = {"name": display("Nom: ")}
+    tournament_dict.update({"place": display("Lieu: ")})
+    tournament_dict.update({"starting_date": display("Date de début: ", "date")})
+    tournament_dict.update({"ending_date": display("Date de fin: ", "date", "", tournament_dict["starting_date"])})
+    tournament_dict.update({"kind": type_str()})
+    print(f'type : {tournament_dict["kind"]}')
+    tournament_dict.update({"number_of_round": display("Nombre de ronde: ", "int")})
+    tournament_dict.update({"description": display("Description: ")})
+    return make_a_tournament(tournament_dict)
+
+
+def display_make_a_player():
+    os.system('cls||clear')
+    display_head_menu("Création de joueur")
+    player_dict = {"lastname": display("Nom: ")}
+    player_dict.update({"firstname": display("prénom: ")})
+    player_dict.update({"gender": display("genre : (homme/femme)", "gender")})
+    player_dict.update({"date_of_birth": display("Date de naissance: ", "date")})
+    player_dict.update({"ranking": display("Classement : ", "natural")})
+    return make_a_player(player_dict)
+
+
+if __name__ == "__main__":
+    print("can't be run")
