1533f7cb876d8cac66e9b2b863cd90f97c1f9988
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-03-15 16:13:54 +0100

    Add files via upload
    version soutenance

--- view/display_instance_making.py
+++ view/display_instance_making.py
@@ -4,10 +4,12 @@ import os
 
 from controller.Display import display, type_str
 from controller.building import make_a_tournament, make_a_player
+
 from view.tools import display_head_menu
 
 
 def display_make_a_tournament():
+    """ Display the tournament making menu, using no attribute"""
     os.system('cls||clear')
     display_head_menu("tournament Building")
     tournament_dict = {"name": display("Nom: ")}
@@ -15,18 +17,19 @@ def display_make_a_tournament():
     tournament_dict.update({"starting_date": display("Date de début: ", "date")})
     tournament_dict.update({"ending_date": display("Date de fin: ", "date", "", tournament_dict["starting_date"])})
     tournament_dict.update({"kind": type_str()})
-    print(f'type : {tournament_dict["kind"]}')
+    print(f'Type : {tournament_dict["kind"]}')
     tournament_dict.update({"number_of_round": display("Nombre de ronde: ", "int")})
     tournament_dict.update({"description": display("Description: ")})
     return make_a_tournament(tournament_dict)
 
 
 def display_make_a_player():
+    """ Display the player making menu, using no attribute"""
     os.system('cls||clear')
     display_head_menu("Création de joueur")
     player_dict = {"lastname": display("Nom: ")}
-    player_dict.update({"firstname": display("prénom: ")})
-    player_dict.update({"gender": display("genre : (homme/femme)", "gender")})
+    player_dict.update({"firstname": display("Prénom: ")})
+    player_dict.update({"gender": display("Genre : (homme/femme)", "gender")})
     player_dict.update({"date_of_birth": display("Date de naissance: ", "date")})
     player_dict.update({"ranking": display("Classement : ", "natural")})
     return make_a_player(player_dict)
