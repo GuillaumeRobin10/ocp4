63f79c891fe6467a21c252faa3d6b8ae64857d3a
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-03-17 02:09:37 +0100

    v 1

---
+++ controller/loading.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from controller.setting import db
+
+
+def get_identity(what_we_looking_for):
+    """take a table on attribute and return the 1st free id """
+    list_of_identity = []
+    identity = 1
+    table = db.table(what_we_looking_for).all()
+    for i in range(len(table)):
+        list_of_identity.append(table[i]["identity"])
+    while identity in list_of_identity:
+        identity += 1
+    return identity
+
+
+def scooting_player():
+    menu = {"cases": [], "string": ""}
+    listing = db.table("players").all()
+    for i in range(len(listing)):
+        menu["cases"].append(str(i+1))
+        menu["string"] += f"{listing[i]['lastname']} {listing[i]['firstname']} ({i + 1})\n"
+    if menu["string"] == "":
+        return 0
+    return menu
+
+
+def scooting_tournament():
+    menu = {"cases": [], "string": ""}
+    listing = db.table("tournament").all()
+    for i in range(len(listing)):
+        menu["cases"].append(str(i+1))
+        menu["string"] += f"{listing[i]['name']} {listing[i]['starting_date']} ({i + 1})\n"
+    if menu["string"] == "":
+        return 0
+    return menu
+
+
+def loading_tournament(identity):
+    listing = db.table("tournament").all()
+    tournament_to_load = listing[int(identity)-1]
+    return {"identity": tournament_to_load["identity"],
+            "name": tournament_to_load["name"],
+            "place": tournament_to_load["place"],
+            "starting_date": tournament_to_load["starting_date"],
+            "ending_date": tournament_to_load["ending_date"],
+            "kind": tournament_to_load["kind"],
+            "description": tournament_to_load["description"],
+            "number_of_round": tournament_to_load["number_of_round"],
+            "players_id": tournament_to_load["players_id"]}
+
+
+def loading_player(identity):
+    listing = db.table("players").all()
+    player_to_load = listing[int(identity)-1]
+    return {"lastname": player_to_load["lastname"],
+            "firstname": player_to_load["firstname"],
+            "date_of_birth": player_to_load["date_of_birth"],
+            "gender": player_to_load["gender"],
+            "ranking": player_to_load["ranking"],
+            "identity": player_to_load["identity"],
+            "tournament_point": player_to_load["tournament_point"]}
+
+
+if __name__ == "__main__":
+    print("can't be run")
