a34247faf3bb053f6de4af62d97a19bf66ef9b14
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-03-17 02:08:02 +0100

    v 1

--- controller/building.py
+++ controller/building.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from model.tournament import Tournament
-from model.player import Player
-
 from controller.loading import get_identity
+from controller.setting import current_tournament, players, current_player
 
 
 def make_a_tournament(tournament_dict):
-    tournament = Tournament()
-    tournament.edit_name(tournament_dict['name'])
-    tournament.edit_place(tournament_dict['place'])
-    tournament.edit_starting_date(tournament_dict['starting_date'])
-    tournament.edit_ending_date(tournament_dict['ending_date'])
-    tournament.edit_kind(tournament_dict['kind'])
-    tournament.edit_number_of_round(tournament_dict['number_of_round'])
-    tournament.edit_description(tournament_dict['description'])
-    tournament.edit_id(get_identity("tournament"))
-    return tournament
+    current_tournament.edit_name(tournament_dict['name'])
+    current_tournament.edit_place(tournament_dict['place'])
+    current_tournament.edit_starting_date(tournament_dict['starting_date'])
+    current_tournament.edit_ending_date(tournament_dict['ending_date'])
+    current_tournament.edit_kind(tournament_dict['kind'])
+    current_tournament.edit_number_of_round(tournament_dict['number_of_round'])
+    current_tournament.edit_description(tournament_dict['description'])
+    current_tournament.edit_id(get_identity("tournament"))
 
 
 def make_a_player(player_dict):
-    player = Player()
-    player.edit_lastname(player_dict['lastname'])
-    player.edit_firstname(player_dict['firstname'])
-    player.edit_date_of_birth(player_dict['date_of_birth'])
-    player.edit_gender(player_dict['gender'])
-    player.edit_ranking(player_dict['ranking'])
-    player.edit_identity(get_identity("players"))
-    return player
+    if len(players) < 8:
+        current_player.edit_lastname(player_dict["lastname"])
+        current_player.edit_firstname(player_dict["firstname"])
+        current_player.edit_date_of_birth(player_dict["date_of_birth"])
+        current_player.edit_gender(player_dict["gender"])
+        current_player.edit_ranking(player_dict["ranking"])
+        current_player.edit_identity(get_identity("players"))
+        players.append(current_player)
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
