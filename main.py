60164af27eaf00d9c0531dd28aa338f25063777c
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-04-09 15:18:52 +0200

    v 1.0.1.0

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from view.main import main_view
+from controller.setting import players, number_of_player
 
+from view.home_menu import first_menu_action
+from view.players_menu import players_menu
+from view.rounding_menu import in_round_menu
 
 if __name__ == "__main__":
-    main_view()
-
-
-
-
-
-
+    first_menu_action()
+    while len(players) < number_of_player:
+        players_menu()
+    in_round_menu()
 
