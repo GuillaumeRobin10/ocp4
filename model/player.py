da421ff29b0cb67dfeb2d2e5815156651fa8cb53
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-04-09 15:48:03 +0200

    v 1.0.1.0

---
+++ model/player.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Player:
+    def __init__(self, lastname="", firstname="", date_of_birth="", gender="", ranking=0, identity=""):
+        self.identity = identity
+        self.lastname = lastname
+        self.firstname = firstname
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+        self.ranking = ranking
+        self.tournament_point = {}
+
+    def edit_identity(self, identity):
+        self.identity = identity
+
+    def edit_lastname(self, lastname):
+        self.lastname = lastname
+
+    def edit_firstname(self, firstname):
+        self.firstname = firstname
+
+    def edit_date_of_birth(self, date_of_birth):
+        self.date_of_birth = date_of_birth
+
+    def edit_gender(self, gender):
+        self.gender = gender
+
+    def edit_ranking(self, ranking):
+        ranking = abs(ranking)
+        self.ranking = ranking
+
+    def add_ranking_point(self, ranking_added):
+        ranking_added = abs(ranking_added)
+        self.ranking += ranking_added
+
+    def remove_ranking_point(self, ranking_removed):
+        ranking_removed = abs(ranking_removed)
+        if (self.ranking - ranking_removed) >= 0:
+            self.ranking -= ranking_removed
+        else:
+            print("modification impossible, le classement ne peut pas être inférieur a 0")
+
+    def add_tournament_point(self, tournament_identity, point_to_add):
+        try:
+            self.tournament_point[tournament_identity] += point_to_add
+        except KeyError:
+            self.tournament_point = {tournament_identity: point_to_add}
+
+    @property
+    def serialized_player(self):
+        serialized = {"lastname": self.lastname,
+                      "firstname": self.firstname,
+                      "date_of_birth": self.date_of_birth,
+                      "gender": self.gender,
+                      "ranking": self.ranking,
+                      "identity": self.identity,
+                      "tournament_point": self.tournament_point}
+        return serialized
+
+
+if __name__ == "__main__":
+    print("can't be run")
---
+++ model/tournament.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Tournament:
+    def __init__(self, identity="1", name="", place="", starting_date="", ending_date="", kind="", description="",
+                 number_of_round=4, players_id=None):
+        self.identity = identity
+        self.name = name
+        self.place = place
+        self.starting_date = starting_date
+        self.ending_date = ending_date
+        self.kind = kind
+        self.description = description
+        self.number_of_round = number_of_round
+        self.players_id = players_id
+        self.rounds = []
+        self.players_id_sorted = []
+        self.serialized_tournament = {}
+
+    def edit_id(self, identity):
+        self.identity = identity
+
+    def edit_name(self, name):
+        self.name = name
+
+    def edit_place(self, place):
+        self.place = place
+
+    def edit_starting_date(self, starting_date):
+        self.starting_date = starting_date
+
+    def edit_ending_date(self, ending_date):
+        self.ending_date = ending_date
+
+    def edit_kind(self, kind):
+        self.kind = kind
+
+    def edit_number_of_round(self, number_of_round):
+        self.number_of_round = number_of_round
+
+    def edit_description(self, description):
+        self.description = description
+
+    def edit_players_id(self, player_id):
+        self.players_id.append(player_id)
+
+    def add_a_round(self, a_round):
+        self.rounds.append(a_round)
+
+    def edit_players_id_sorted(self, player_id):
+        self.players_id_sorted.append(player_id)
+
+    def serialization(self):
+        self.serialized_tournament = {"identity": self.identity,
+                                      "name": self.name,
+                                      "place": self.place,
+                                      "starting_date": self.starting_date,
+                                      "ending_date": self.ending_date,
+                                      "kind": self.kind,
+                                      "description": self.description,
+                                      "number_of_round": self.number_of_round,
+                                      "players_id": self.players_id}
+
+
+if __name__ == "__main__":
+    print("can't be run")
